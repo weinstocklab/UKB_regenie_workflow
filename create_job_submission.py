56e0a61b4229febbe4899e0b67c5b0ccb15d7349
joshua.s.weinstock@gmail.com <Josh Weinstock>
2026-03-09 10:52:15 -0400

    formatting changes

--- create_job_submission.py
+++ create_job_submission.py
@@ -6,29 +6,29 @@ config_file = sys.argv[1]
 with open(config_file) as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-cost_limit                   = cfg["cost_limit"]
-project                      = cfg["project"]
-priority                     = cfg["priority"]
-step1_pvar                   = cfg["step1_pvar"]
-step1_psam                   = cfg["step1_psam"]
-step1_pgen                   = cfg["step1_pgen"]
-step1_prefix                 = cfg["step1_prefix"]
-covariates                   = cfg["covariates"]
-phenotypes                   = cfg["phenotypes"]
-covariate_string             = cfg["covariate_string"]
+cost_limit = cfg["cost_limit"]
+project = cfg["project"]
+priority = cfg["priority"]
+step1_pvar = cfg["step1_pvar"]
+step1_psam = cfg["step1_psam"]
+step1_pgen = cfg["step1_pgen"]
+step1_prefix = cfg["step1_prefix"]
+covariates = cfg["covariates"]
+phenotypes = cfg["phenotypes"]
+covariate_string = cfg["covariate_string"]
 categorical_covariate_string = cfg["categorical_covariate_string"]
-final_folder                 = cfg["final_folder"]
-concatenate                  = cfg["concatenate"]
-fix_step2_header_for_rap     = cfg["fix_step2_header_for_rap"]
-workflow                     = cfg["workflow"]
-step2_block_size             = cfg["step2_block_size"]
-minMAC                       = cfg["minMAC"]
-step2_chunk_manifest         = cfg["step2_chunk_manifest"]
-plink2_binary                = cfg["plink2_binary"]
+final_folder = cfg["final_folder"]
+concatenate = cfg["concatenate"]
+fix_step2_header_for_rap = cfg["fix_step2_header_for_rap"]
+workflow = cfg["workflow"]
+step2_block_size = cfg["step2_block_size"]
+minMAC = cfg["minMAC"]
+step2_chunk_manifest = cfg["step2_chunk_manifest"]
+plink2_binary = cfg["plink2_binary"]
 
-if __name__ == '__main__':
-
-    print('uv run dx run {workflow} \
+if __name__ == "__main__":
+    print(
+        'uv run dx run {workflow} \
      -istage-common.step1_pvar={step1_pvar} \
      -istage-common.step1_pgen={step1_pgen} \
      -istage-common.step1_psam={step1_psam} \
@@ -49,23 +49,23 @@ if __name__ == '__main__':
      --cost-limit {cost} \
      -y \
      --brief'.format(
-                 workflow = workflow,
-                 step1_pvar=step1_pvar,
-                 step1_pgen=step1_pgen,
-                 step1_psam=step1_psam,
-                 step1_prefix=step1_prefix,
-                 covariate_string=covariate_string,
-                 categorical_covariate_string=categorical_covariate_string,
-                 covariates=covariates,
-                 phenotypes=phenotypes,
-                 minMAC = minMAC,
-                 step2_block_size = step2_block_size,
-                 step2_chunk_manifest = step2_chunk_manifest,
-                 concatenate=concatenate,
-                 fix_step2_header_for_rap=fix_step2_header_for_rap,
-                 plink2_binary=plink2_binary,
-                 final_folder= final_folder,
-                 priority = priority,
-                 cost = cost_limit,
-                 )
+            workflow=workflow,
+            step1_pvar=step1_pvar,
+            step1_pgen=step1_pgen,
+            step1_psam=step1_psam,
+            step1_prefix=step1_prefix,
+            covariate_string=covariate_string,
+            categorical_covariate_string=categorical_covariate_string,
+            covariates=covariates,
+            phenotypes=phenotypes,
+            minMAC=minMAC,
+            step2_block_size=step2_block_size,
+            step2_chunk_manifest=step2_chunk_manifest,
+            concatenate=concatenate,
+            fix_step2_header_for_rap=fix_step2_header_for_rap,
+            plink2_binary=plink2_binary,
+            final_folder=final_folder,
+            priority=priority,
+            cost=cost_limit,
+        )
     )
